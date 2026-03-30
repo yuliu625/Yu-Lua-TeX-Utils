2f75200c20a90145b464cc1a4125f10f0fbc50fe
yu <yul1024@outlook.com>
2026-03-30 12:20:01 +0800

    build: add l3build file

---
+++ build.lua
@@ -0,0 +1,25 @@
+-- build.lua basic configurations
+
+-- basic information
+module = 'yu-configs'
+pkgname = 'yu-configs'
+
+
+-- package codes
+-- -- TDS: tex/latex/
+installfiles = {'yu-configs.sty', 'yu_logic.lua',}
+
+
+-- document files
+-- -- doc/
+docfiles = {'docfiles/*.tex', 'README.md', 'README.zh-CN.md',}
+
+
+-- source files
+-- -- source/  .dtx & .ins
+sourcefiles = {}
+
+
+-- dev scraps
+excludefiles = {}
+
