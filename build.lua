900ff4226e75f6c4fb9d2e0811c5a61d31b580ec
yu <yul1024@outlook.com>
2026-08-12 21:03:31 +0800

    build: add l3build file

--- build.lua
+++ build.lua
@@ -1,25 +1,93 @@
--- build.lua basic configurations
+-- build.lua
+-- l3build automation configuration file
 
--- basic information
+-- =====
+-- Project Metadata
+-- =====
 module = 'yu-setup'
-pkgname = 'yu-setup'
+ctanpkg = 'yu-setup'
+license = 'lppl1.3c'
+version = 'v0.0.1'
+date = '2026/03/01'
+author = 'Yu'
 
 
--- package codes
+-- =====
+-- Directory Structure
+-- =====
+sourcefiledir = "."
+docfiledir = "docfiles"
+testfiledir = "testfiles"
+builddir = "build"
+
+
+-- =====
+-- File Rules
+-- =====
+-- Core source files and unpacking drivers
+sourcefiles = {
+    "*.dtx",
+    "*.ins",
+}
+unpackfiles = {
+    "*.ins",
+}
+-- Documentation files to be installed into the TDS structure
 -- -- TDS: tex/latex/
-installfiles = {'yu-setup.sty', 'yu_logic.lua',}
+installfiles = {
+    "yu-setup.sty",
+    "yu_utils.lua",
+}
+-- Documentation files to be installed into the TDS structure
+-- -- doc/latex/
+docfiles = {
+    "*.tex",
+    "README.md",
+    "README.zh-CN.md",
+}
+-- Main master document used to typeset the user manual
+typesetfiles = {
+    "yu-setup-doc.tex",
+}
+-- Files treated as text during packaging and line-ending conversions
+textfiles = {
+    "README.md",
+    "README.zh-CN.md",
+}
+-- Files treated as binary assets
+-- -- Preserved without encoding/EOL modifications
+binaryfiles = {
+    "*.pdf",
+    "*.png",
+}
+-- Patterns for files to exclude from build and release archives
+excludefiles = {
+  "*~",
+  "*.tmp",
+  "*.log",
+  ".git*",
+}
 
 
--- document files
--- -- doc/
-docfiles = {'docfiles/*.tex', 'README.md', 'README.zh-CN.md',}
+-- =====
+-- Engines & Typesetting
+-- =====
+-- Default engine for unpacking and standard operations
+stdengine = 'pdftex'
+-- Target TeX engines used for automated regression testing
+checkengines = {
+  'pdftex',
+  'xetex',
+  'luatex',
+}
+-- Engine configuration for typesetting documentation
+typesetengines = {
+    'luatex',
+}
+typesetexe = 'lualatex'
 
 
--- source files
--- -- src/  .dtx & .ins
-sourcefiles = {}
-
-
--- dev scraps
-excludefiles = {}
+-- =====
+-- Hooks
+-- =====
 
